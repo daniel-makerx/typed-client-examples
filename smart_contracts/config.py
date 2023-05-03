3871fdad682d1f7caed602c8807fb4745b73b157
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-03 17:43:18 +0800

    feat: add VotingRoundApp client (wip)

--- smart_contracts/config.py
+++ smart_contracts/config.py
@@ -1,8 +1,8 @@
 import logging
 
-from smart_contracts import helloworld
+from smart_contracts import helloworld, voting
 
 logger = logging.getLogger(__name__)
 
 # define contracts to build and/or deploy
-contracts = [helloworld.app]
+contracts = [helloworld.app, voting.app]
