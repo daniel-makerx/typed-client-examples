17f8017a237bc1c282ddf7b2c126eb6a7a09d5ac
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-03 11:33:31 +0800

    build: disable removing artifacts tree

--- smart_contracts/helpers/build.py
+++ smart_contracts/helpers/build.py
@@ -1,6 +1,5 @@
 import logging
 from pathlib import Path
-from shutil import rmtree
 
 import beaker
 
@@ -9,8 +8,6 @@ logger = logging.getLogger(__name__)
 
 def build(output_dir: Path, app: beaker.Application) -> Path:
     output_dir = output_dir.resolve()
-    if output_dir.exists():
-        rmtree(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     logger.info(f"Exporting {app.name} to {output_dir}")
     specification = app.build()
