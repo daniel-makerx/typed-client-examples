e01a694f03dfc44eedc29f3efe134081f12a6b16
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-05 17:12:14 +0800

    Add python helloworld typed client

--- smart_contracts/artifacts/HelloWorldApp/client.py
+++ smart_contracts/artifacts/HelloWorldApp/client.py
@@ -48,7 +48,7 @@ class HelloWorldAppClient:
         suggested_params: algosdk.transaction.SuggestedParams | None = None,
         template_values: algokit_utils.TemplateValueMapping | None = None,
     ):
-        app_spec_path = pathlib.Path(__file__).parent / "application.json"
+        app_spec_path = pathlib.Path(__file__).parent / "application.json"  # TODO: embed
         self.app_spec = algokit_utils.ApplicationSpecification.from_json(app_spec_path.read_text())
 
         # calling full __init__ signature, so ignoring mypy warning about overloads
@@ -65,5 +65,90 @@ class HelloWorldAppClient:
             template_values=template_values,
         )
 
-    def hello(self, name: str) -> str:
-        return self.app_client.call("hello(string)string", name=name).return_value
+    def hello(  # from $.contract.methods[name="hello"]
+        self,
+        name: str,
+        transaction_parameters: algokit_utils.CommonCallParameters
+        | algokit_utils.CommonCallParametersDict
+        | None = None,
+    ) -> algokit_utils.ABITransactionResponse[str]:
+        # convert parameters into OnCompleteCallParameters with NoOpOC
+        parameters = (
+            transaction_parameters.__dict__
+            if isinstance(transaction_parameters, algokit_utils.CommonCallParameters)
+            else (transaction_parameters or {})
+        )
+        transaction_parameters = algokit_utils.OnCompleteCallParameters(
+            **parameters, on_complete=algosdk.transaction.OnComplete.NoOpOC
+        )
+        # call is used because the ABI method call config for hello is no_op
+        # from $.hints["hello(string)string""].call_config
+        return self.app_client.call("hello(string)string", name=name, transaction_parameters=transaction_parameters)
+
+    def create(  # from $.bare_call_config.no_op == 'CREATE'
+        self,
+        args: None = None,  # not required in this scenario, so takes None and defaults to None. Included so that if the
+        # smart contract changes to have ABI parameters the signature will still be similar
+        transaction_parameters: algokit_utils.CreateCallParameters
+        | algokit_utils.CreateCallParametersDict
+        | None = None,
+    ) -> algokit_utils.TransactionResponse:
+        return self.app_client.create(
+            call_abi_method=False,  # False is used to indicate we want to call the bare_method, not an ABI method
+            transaction_parameters=transaction_parameters,
+        )
+
+    def delete(  # from $.bare_call_config.delete_application == 'CALL'
+        self,
+        args: None = None,  # not required in this scenario, so takes None and defaults to None. Included so that if the
+        # smart contract changes to have ABI parameters the signature will still be similar
+        transaction_parameters: algokit_utils.CommonCallParameters
+        | algokit_utils.CommonCallParametersDict
+        | None = None,
+    ) -> algokit_utils.TransactionResponse:
+        return self.app_client.delete(
+            call_abi_method=False,  # False is used to indicate we want to call the bare_method, not an ABI method
+            transaction_parameters=transaction_parameters,
+        )
+
+    def update(  # from $.bare_call_config.update_application == 'CALL'
+        self,
+        args: None = None,  # not required in this scenario, so takes None and defaults to None. Included so that if the
+        # smart contract changes to have ABI parameters the signature will still be similar
+        transaction_parameters: algokit_utils.CommonCallParameters
+        | algokit_utils.CommonCallParametersDict
+        | None = None,
+    ) -> algokit_utils.TransactionResponse:
+        return self.app_client.update(
+            call_abi_method=False,  # False is used to indicate we want to call the bare_method, not an ABI method
+            transaction_parameters=transaction_parameters,
+        )
+
+    def deploy(
+        self,
+        version: str | None = None,
+        *,
+        signer: TransactionSigner | None = None,
+        sender: str | None = None,
+        allow_update: bool | None = None,
+        allow_delete: bool | None = None,
+        on_update: algokit_utils.OnUpdate = algokit_utils.OnUpdate.Fail,
+        on_schema_break: algokit_utils.OnSchemaBreak = algokit_utils.OnSchemaBreak.Fail,
+        template_values: algokit_utils.TemplateValueMapping | None = None,
+        create_args: algokit_utils.ABICallArgs | None = None,
+        update_args: algokit_utils.ABICallArgs | None = None,
+        delete_args: algokit_utils.ABICallArgs | None = None,
+    ) -> algokit_utils.DeployResponse:
+        return self.app_client.deploy(
+            version,
+            signer=signer,
+            sender=sender,
+            allow_update=allow_update,
+            allow_delete=allow_delete,
+            on_update=on_update,
+            on_schema_break=on_schema_break,
+            template_values=template_values,
+            create_args=create_args,
+            update_args=update_args,
+            delete_args=delete_args,
+        )
