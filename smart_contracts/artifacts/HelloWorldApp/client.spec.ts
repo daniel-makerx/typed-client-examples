edf15c54e7dbc086edb4462bc94d748e036cc52c
Rob Moore <rob.moore@makerx.com.au>
2023-05-09 01:28:20 +0800

    Added example of tuple invocation

--- smart_contracts/artifacts/HelloWorldApp/client.spec.ts
+++ smart_contracts/artifacts/HelloWorldApp/client.spec.ts
@@ -22,7 +22,10 @@ describe('hello world typed client', () => {
     const response = await client.hello({ name: 'World' })
     expect(response.return).toBe('Hello, World')
 
-    const response2 = await client.hello_world_check({ name: 'World' })
-    expect(response2.return).toBe(undefined)
+    const response2 = await client.hello(['World!'])
+    expect(response2.return).toBe('Hello, World!')
+
+    const response3 = await client.hello_world_check({ name: 'World' })
+    expect(response3.return).toBe(undefined)
   })
 })
