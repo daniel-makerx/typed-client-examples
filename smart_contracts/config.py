36e912f677eee6f9102e9d5ddc0d5f9c536b818b
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-08 12:25:30 +0800

    add smart contract with multiple create methods

--- smart_contracts/config.py
+++ smart_contracts/config.py
@@ -1,8 +1,8 @@
 import logging
 
-from smart_contracts import helloworld, voting
+from smart_contracts import helloworld, lifecycle, voting
 
 logger = logging.getLogger(__name__)
 
 # define contracts to build and/or deploy
-contracts = [helloworld.app, voting.app]
+contracts = [helloworld.app, voting.app, lifecycle.app]
