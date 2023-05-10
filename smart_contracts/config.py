e151b7643fe4f9856438e655ce7e8f1d63e89c02
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-10 13:46:36 +0800

    feat: add TestingApp

--- smart_contracts/config.py
+++ smart_contracts/config.py
@@ -1,8 +1,8 @@
 import logging
 
-from smart_contracts import helloworld, lifecycle, voting
+from smart_contracts import helloworld, lifecycle, testing_app, voting
 
 logger = logging.getLogger(__name__)
 
 # define contracts to build and/or deploy
-contracts = [helloworld.app, voting.app, lifecycle.app]
+contracts = [helloworld.app, voting.app, lifecycle.app, testing_app.app]
