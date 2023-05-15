820ea7105172c9ef4b63add5dbf8ba18881d3a85
Tristan Menzel <tristanmenzel@gmail.com>
2023-05-15 16:46:25 -0700

    Make the cli experience more robust and user friendly

--- algokit-client-generator-ts/src/cli.ts
+++ algokit-client-generator-ts/src/cli.ts
@@ -4,27 +4,47 @@ import * as path from 'path'
 import { generate } from './client/generate'
 import { writeDocumentPartsToStream } from './output/writer'
 import fs from 'fs'
-import { boom } from './util/boom'
+import { colorConsole } from './util/color-console'
 
 export function cli(workingDirectory: string, args: string[]) {
   const program = new Command()
   program
     .command('generate')
-    .description('Generates a TypeScript client for the given application.schema.json file')
-    .option('-a --application <path>', 'Specifies the application.json file')
-    .option('-o --output <path>', 'Specifies the output file path')
-    .action(({ application, output }: { application?: string; output?: string }): void => {
-      if (!application) return boom('Please specify a path to the application spec')
-      if (!output) return boom('Please specify an output path for the client')
-      const spec = load(path.resolve(workingDirectory, application))
-
+    .description('Generates a TypeScript client for the given application.json file')
+    .requiredOption('-a --application <path>', 'Specifies the application.json file')
+    .requiredOption('-o --output <path>', 'Specifies the output file path')
+    .action(({ application, output }: { application: string; output: string }): void => {
+      const resolvedAppJsonPath = path.resolve(workingDirectory, application)
+      const resolvedOutPath = path.resolve(workingDirectory, output)
+      const resolvedOutDir = path.dirname(resolvedOutPath)
+      colorConsole.info`Reading application.json file from path ${resolvedAppJsonPath}`
+      const spec = load(resolvedAppJsonPath)
+      colorConsole.info`Generating TS client for ${spec.contract.name}`
       const parts = generate(spec)
-
-      const file = fs.createWriteStream(path.resolve(workingDirectory, output), {
+      if (!fs.existsSync(resolvedOutDir)) {
+        colorConsole.warn`Output directory ${resolvedOutDir} does not exist and will be created.`
+        fs.mkdirSync(resolvedOutDir, { recursive: true })
+      }
+      colorConsole.info`Writing TS client to ${resolvedOutPath}`
+      const file = fs.createWriteStream(resolvedOutPath, {
         flags: 'w',
       })
       writeDocumentPartsToStream(parts, file)
+      colorConsole.success`Operation completed successfully`
     })
-
-  program.parse(args)
+    .configureOutput({
+      writeErr(str: string) {
+        colorConsole.error`${str}`
+      },
+    })
+  try {
+    program.parse(args)
+  } catch (err) {
+    if (err instanceof Error) {
+      colorConsole.error`Unhandled error: \n\n${err.stack}`
+    } else {
+      colorConsole.error`Unhandled error: \n\n${err}`
+    }
+    process.exit(-1)
+  }
 }
