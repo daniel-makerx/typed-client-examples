e37718fb3052e4282fdd3a635251c08778c8ae3e
inaie ignacio <inaie.ignacio@makerx.com.au>
2023-05-16 15:37:43 +0800

    Fixing tests

--- smart_contracts/artifacts/VotingRoundApp/test_client.py
+++ smart_contracts/artifacts/VotingRoundApp/test_client.py
@@ -5,45 +5,15 @@ import random
 import algokit_utils
 import algosdk
 import pytest
-from algokit_utils import Account, get_localnet_default_account
+from algokit_utils import get_localnet_default_account
 from algosdk.atomic_transaction_composer import TransactionWithSigner
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
 from smart_contracts.artifacts.VotingRoundApp.client import CreateArgs, DeployCallCreateAbiArgs, VotingRoundAppClient
 
-# @pytest.fixture()
-# def voting_round_app_client(
-#     algod_client: AlgodClient, indexer_client: IndexerClient, new_account: Account
-# ) -> VotingRoundAppClient:
-#     client = VotingRoundAppClient(
-#         algod_client=algod_client,
-#         indexer_client=indexer_client,
-#         creator=new_account,
-#     )
-#     return client
 
-
-@pytest.fixture(scope="session")
-def voting_round_app_client(algod_client: AlgodClient, indexer_client: IndexerClient) -> VotingRoundAppClient:
-    client = VotingRoundAppClient(
-        algod_client=algod_client,
-        indexer_client=indexer_client,
-        creator=get_localnet_default_account(algod_client),
-    )
-    client.deploy(allow_delete=True, template_values={"VALUE": 1})
-    return client
-
-
-@pytest.fixture
-def deploy_voting_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient, new_account: Account
-) -> VotingRoundAppClient:
-    client = VotingRoundAppClient(
-        algod_client=algod_client,
-        indexer_client=indexer_client,
-        creator=get_localnet_default_account(algod_client),
-    )
+def get_create_args(algod_client: AlgodClient) -> CreateArgs:
     quorum = math.ceil(random.randint(1, 9) * 1000)
     current_date = datetime.datetime.now()
     question_counts = [0 for i in range(0, 10)]
@@ -66,10 +36,35 @@ def deploy_voting_client(
         nft_image_url="ipfs://cid",
         option_counts=question_counts,
     )
+    return create_args
+
+
+def get_deploy_create_args(algod_client: AlgodClient) -> DeployCallCreateAbiArgs:
     sp = algod_client.suggested_params()
     sp.fee = algosdk.util.algos_to_microalgos(4)
     sp.flat_fee = True
-    client.deploy(allow_delete=True, create_args=DeployCallCreateAbiArgs(args=create_args, suggested_params=sp))
+    return DeployCallCreateAbiArgs(args=get_create_args(algod_client=algod_client), suggested_params=sp)
+
+
+@pytest.fixture(scope="session")
+def voting_round_app_client(algod_client: AlgodClient, indexer_client: IndexerClient) -> VotingRoundAppClient:
+    client = VotingRoundAppClient(
+        algod_client=algod_client,
+        indexer_client=indexer_client,
+        creator=get_localnet_default_account(algod_client),
+    )
+    # client.deploy(allow_delete=True, template_values={"VALUE": 1})
+    return client
+
+
+@pytest.fixture
+def deploy_voting_client(algod_client: AlgodClient, indexer_client: IndexerClient) -> VotingRoundAppClient:
+    client = VotingRoundAppClient(
+        algod_client=algod_client,
+        indexer_client=indexer_client,
+        creator=get_localnet_default_account(algod_client),
+    )
+    client.deploy(allow_delete=True, create_args=get_deploy_create_args(algod_client=algod_client))
 
     return client
 
@@ -110,39 +105,28 @@ def test_vote(deploy_voting_client: VotingRoundAppClient, algod_client: AlgodCli
     assert response.return_value is None
 
 
-def test_create(voting_round_app_client: VotingRoundAppClient) -> None:
-    quorum = math.ceil(random.randint(1, 9) * 1000)
-    current_date = datetime.datetime.now()
-    question_counts = [0 for i in range(0, 10)]
-    vote_id = str(int(current_date.strftime("%Y%m%d%H%M%S")))
+def test_create(voting_round_app_client: VotingRoundAppClient, algod_client: AlgodClient) -> None:
+    voting_round_app_client.deploy(allow_delete=True, template_values={"VALUE": 1})
+    voting_round_app_client.create(args=get_create_args(algod_client=algod_client))
 
-    create_args = CreateArgs(
-        vote_id=vote_id,
-        metadata_ipfs_cid="cid",
-        start_time=int(current_date.strftime("%H%M%S")),
-        end_time=int(current_date.strftime("%H%M%S")) + 1000,
-        quorum=quorum,
-        snapshot_public_key=b"key",
-        nft_image_url="ipfs://cid",
-        option_counts=question_counts,
-    )
-    voting_round_app_client.create(args=create_args)
-
-    # assert response.return_value == "Hello, World"
+    # assert response.return_value voting_round_app_client== "Hello, World"
 
 
-def test_boostrap(deploy_voting_client: VotingRoundAppClient) -> None:
-    from_account = algokit_utils.get_localnet_default_account(deploy_voting_client.app_client.algod_client)
+def test_boostrap(voting_round_app_client: VotingRoundAppClient, algod_client: AlgodClient) -> None:
+    voting_round_app_client.deploy(allow_delete=True, create_args=get_deploy_create_args(algod_client=algod_client))
+
+    from_account = algokit_utils.get_localnet_default_account(voting_round_app_client.app_client.algod_client)
     payment = algosdk.transaction.PaymentTxn(
         sender=from_account.address,
-        receiver=deploy_voting_client.app_client.app_address,
-        amt=200_000,
+        receiver=voting_round_app_client.app_client.app_address,
+        amt=(100000 * 2) + 1000 + 2500 + 400,
         note=b"Bootstrap payment",
-        sp=deploy_voting_client.app_client.algod_client.suggested_params(),
+        sp=voting_round_app_client.app_client.algod_client.suggested_params(),
     )
 
-    result = deploy_voting_client.bootstrap(
-        fund_min_bal_req=TransactionWithSigner(payment, deploy_voting_client.app_client.signer)
+    voting_round_app_client.bootstrap(
+        fund_min_bal_req=TransactionWithSigner(payment, voting_round_app_client.app_client.signer),
+        transaction_parameters=algokit_utils.TransactionParameters(boxes=[(0, "V")]),
     )
 
-    assert result.confirmed_round is None
+    # assert result.confirmed_round is None
