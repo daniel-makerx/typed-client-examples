7fd99f37487825b4842aec08e0f0d0d1966adb53
inaie ignacio <inaie.ignacio@makerx.com.au>
2023-05-17 09:43:25 +0800

    Fix tests

--- smart_contracts/artifacts/VotingRoundApp/test_client.py
+++ smart_contracts/artifacts/VotingRoundApp/test_client.py
@@ -5,8 +5,8 @@ import random
 import algokit_utils
 import algosdk
 import pytest
-from algokit_utils import get_localnet_default_account
-from algosdk.atomic_transaction_composer import TransactionWithSigner
+from algokit_utils import Account, get_localnet_default_account
+from algosdk.atomic_transaction_composer import AccountTransactionSigner, TransactionWithSigner
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
@@ -46,12 +46,14 @@ def get_deploy_create_args(algod_client: AlgodClient) -> DeployCallCreateAbiArgs
     return DeployCallCreateAbiArgs(args=get_create_args(algod_client=algod_client), suggested_params=sp)
 
 
-@pytest.fixture(scope="session")
-def voting_round_app_client(algod_client: AlgodClient, indexer_client: IndexerClient) -> VotingRoundAppClient:
+@pytest.fixture
+def voting_round_app_client(
+    algod_client: AlgodClient, indexer_client: IndexerClient, new_account: Account
+) -> VotingRoundAppClient:
     client = VotingRoundAppClient(
         algod_client=algod_client,
         indexer_client=indexer_client,
-        creator=get_localnet_default_account(algod_client),
+        creator=new_account,
     )
     # client.deploy(allow_delete=True, template_values={"VALUE": 1})
     return client
@@ -69,8 +71,11 @@ def deploy_voting_client(algod_client: AlgodClient, indexer_client: IndexerClien
     return client
 
 
-def test_close(deploy_voting_client: VotingRoundAppClient) -> None:
-    deploy_voting_client.close()
+def test_close(deploy_voting_client: VotingRoundAppClient, algod_client: AlgodClient) -> None:
+    sp = algod_client.suggested_params()
+    sp.fee = algosdk.util.algos_to_microalgos(300)
+    sp.flat_fee = True
+    deploy_voting_client.close(transaction_parameters=algokit_utils.TransactionParameters(suggested_params=sp))
 
     # assert response.return_value == "Hello, World"
 
@@ -105,9 +110,22 @@ def test_vote(deploy_voting_client: VotingRoundAppClient, algod_client: AlgodCli
     assert response.return_value is None
 
 
-def test_create(voting_round_app_client: VotingRoundAppClient, algod_client: AlgodClient) -> None:
-    voting_round_app_client.deploy(allow_delete=True, template_values={"VALUE": 1})
-    voting_round_app_client.create(args=get_create_args(algod_client=algod_client))
+def test_create(algod_client: AlgodClient, indexer_client: IndexerClient, new_account: Account) -> None:
+    voting_round_app_client = VotingRoundAppClient(
+        algod_client=algod_client,
+        indexer_client=indexer_client,
+        creator=new_account,
+        template_values={"VALUE": 1, "DELETABLE": 1},
+    )
+
+    sp = algod_client.suggested_params()
+    sp.fee = algosdk.util.algos_to_microalgos(4)
+    sp.flat_fee = True
+
+    voting_round_app_client.create(
+        args=get_create_args(algod_client=algod_client),
+        transaction_parameters=algokit_utils.TransactionParameters(suggested_params=sp),
+    )
 
     # assert response.return_value voting_round_app_client== "Hello, World"
 
@@ -123,9 +141,11 @@ def test_boostrap(voting_round_app_client: VotingRoundAppClient, algod_client: A
         note=b"Bootstrap payment",
         sp=voting_round_app_client.app_client.algod_client.suggested_params(),
     )
+    account = get_localnet_default_account(algod_client)
+    signer = AccountTransactionSigner(account.private_key)
 
     voting_round_app_client.bootstrap(
-        fund_min_bal_req=TransactionWithSigner(payment, voting_round_app_client.app_client.signer),
+        fund_min_bal_req=TransactionWithSigner(txn=payment, signer=signer),
         transaction_parameters=algokit_utils.TransactionParameters(boxes=[(0, "V")]),
     )
 
