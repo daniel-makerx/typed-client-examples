efb8c00814c0953b5332e3f3565a9df82aac8dae
Negar Abbasi <negar.abbasi@makerx.com.au>
2023-05-17 17:17:08 +1000

    some changes intest vote

--- smart_contracts/artifacts/VotingRoundApp/test_client.py
+++ smart_contracts/artifacts/VotingRoundApp/test_client.py
@@ -19,7 +19,7 @@ from smart_contracts.artifacts.VotingRoundApp.client import CreateArgs, DeployCa
 def create_args(algod_client: AlgodClient, voter: tuple[Account, bytes]) -> CreateArgs:
     quorum = math.ceil(random.randint(1, 9) * 1000)
     current_date = datetime.datetime.now()
-    question_counts = [0 for i in range(0, 10)]
+    question_counts = [4]
     vote_id = str(int(current_date.strftime("%Y%m%d%H%M%S")))
 
     health = algod_client.status()
@@ -62,7 +62,7 @@ def deploy_voting_client(
         parameters=algokit_utils.TransferParameters(
             from_account=algokit_utils.get_localnet_default_account(algod_client),
             to_address=new_account.address,
-            micro_algos=algosdk.util.algos_to_microalgos(10000),
+            micro_algos=algosdk.util.algos_to_microalgos(100000),
         ),
     )
 
@@ -86,7 +86,7 @@ def voter(algod_client: AlgodClient) -> tuple[Account, bytes]:
         parameters=algokit_utils.TransferParameters(
             from_account=algokit_utils.get_localnet_default_account(algod_client),
             to_address=voter_account.address,
-            micro_algos=algosdk.util.algos_to_microalgos(100),
+            micro_algos=algosdk.util.algos_to_microalgos(10),
         ),
     )
     return voter_account, signature
@@ -114,41 +114,32 @@ def test_get_preconditions(
 def test_vote(
     deploy_voting_client: VotingRoundAppClient, algod_client: AlgodClient, voter: tuple[Account, bytes]
 ) -> None:
-    from_account = algokit_utils.get_localnet_default_account(algod_client)
+    algokit_utils.get_localnet_default_account(algod_client)
     sp = algod_client.suggested_params()
     sp.fee = 12000
+
     sp.flat_fee = True
     payment = algosdk.transaction.PaymentTxn(
-        sender=from_account.address,
+        sender=voter[0].address,
         receiver=deploy_voting_client.app_client.app_address,
-        amt=200_000,
-        note=b"Bootstrap payment",
-        sp=algod_client.suggested_params(),
+        amt=400 * (32 + 2 + 10 * 1) + 2500,
+        sp=deploy_voting_client.app_client.algod_client.suggested_params(),
     )
-    fund_min_bal_req = TransactionWithSigner(payment, deploy_voting_client.app_client.signer)
-    voter = algosdk.account.generate_account()
-    sender_generate_account = algosdk.account.generate_account()
-    sender_account = algokit_utils.models.Account(
-        private_key=sender_generate_account[0], address=sender_generate_account[1]
-    )
-    transfer = algokit_utils.transfer(
-        client=algod_client,
-        parameters=algokit_utils.TransferParameters(
-            from_account=sender_account,
-            to_address=voter[1],
-            micro_algos=algosdk.util.algos_to_microalgos(1),
-            suggested_params=sp,
-        ),
-    )
-    decoded = algosdk.encoding.decode_address(voter[1])
-    signature = algosdk.logic.teal_sign(private_key=voter[0], contract_addr=transfer, data=decoded)
+    get_localnet_default_account(algod_client)
+    signer = AccountTransactionSigner(voter[0].private_key)
+    fund_min_bal_req = TransactionWithSigner(payment, signer)
     signature = voter[1]
-    answer_ids = [1, 2, 3]
+    answer_ids = [3]
     response = deploy_voting_client.vote(
         fund_min_bal_req=fund_min_bal_req,
         signature=signature,
         answer_ids=answer_ids,
-        transaction_parameters=algokit_utils.TransactionParameters(suggested_params=sp),
+        transaction_parameters=algokit_utils.TransactionParameters(
+            suggested_params=sp,
+            boxes=[(0, "V"), (0, algosdk.encoding.decode_address(voter[0].address))],
+            sender=voter[0].address,
+            signer=AccountTransactionSigner(voter[0].private_key),
+        ),
     )
     assert response.return_value is None
 
